d557501dc3ffdff98c5be57179f337b19dbf1bae
Aadhi1445 <adityaguttula205@gmail.com>
2026-09-07 00:00:24 +0530

    Forms

---
+++ JavaScript/day1/day_1/day_1/day_1.js
@@ -0,0 +1,72 @@
+// var a = 10;
+// var b = 20;
+// var a = 40;
+// // document.write(a + b)
+// console.log(a+b);
+// var a = 10;
+// var a = 20;
+// console.log(a)
+// // we can overide while using var
+
+// var a =5;
+// let b = 20;
+// console.log(a + b);
+// b = 35;
+// console.log(a + b)
+
+// console.log(add(2,5))
+// function add( a, b) {
+//     return a + b
+// }
+// // console.log(add(2,5))
+
+
+// // function add(a , b) {
+// //     return a + b
+// // }
+// // function add(a,b) {
+// //     return a - b
+// // }
+// // console.log(add(10,5))
+
+// function add(a,b) {
+//     return a - b
+// }
+// function add(a , b) {
+//     return a + b
+// }
+// console.log(add(10,5))
+
+// // function sub(a,b) {
+// //     return a - b;
+// // }
+// // const my_fun = function (a , b) {
+// //     return a - b;
+// // }
+// // console.log(sub(5,2))
+// // console.log(sub(8,4))
+
+// function sub(a,b) {
+//     return a - b;
+// }
+// const fun = function  (a , b) {
+//     return a + b;
+// }
+// console.log(sub(5,2))
+// console.log(fun(8,4))
+
+// // // const a = 5
+// // // a = 10
+// // // console.log(a)
+
+// // const a = 5
+// // var a = 10
+// // console.log(a)
+
+// const a = 5
+// let a = 10
+// console.log(a)
+
+// const a = 5
+// a = 10
+// console.log(a)
